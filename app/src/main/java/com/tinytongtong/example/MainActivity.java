5cf5b36abcfdfa92b1ec6283796ba489fe2f9fde
wangjianzhou <tinytongtong@163.com>
2019-08-01 15:25:52 +0800

    first init

---
+++ app/src/main/java/com/tinytongtong/example/MainActivity.java
@@ -0,0 +1,27 @@
+package com.tinytongtong.example;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.tinytongtong.tinyutils.ScreenUtils;
+
+/**
+ * @Description: TinyUtils相关demo
+ *
+ * @Author wangjianzhou
+ * @Date 2019-08-01 15:24
+ * @Version v0.0.1
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.tv);
+        textView.setText(ScreenUtils.getDisplayMetricsInfo(this));
+    }
+}
